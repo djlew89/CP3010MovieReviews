845e72280cab1fe3dea46759154d1f25dfa24855
Dan Lewis <Daniel.Lewis37@ed.cna.nl.ca>
2023-04-10 20:32:27 -0230

    Assignment 5 finished

--- node-express-backend/src/server.js
+++ node-express-backend/src/server.js
@@ -5,7 +5,30 @@ import {MongoClient } from 'mongodb';
 import { fileURLToPath } from 'url';
 import multer from 'multer';
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+
 dotenv.config()
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_CONNECT + '/customers');
+
+}
+
+main();
+
+const infoSchema = new mongoose.Schema({
+  name: String,
+  sport: String,
+  movie: String,
+  email: {type: String, required: true}
+});
+
+const Info = mongoose.model('Info', infoSchema);
+
+
+const jsonParser = bodyParser.json();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
@@ -14,6 +37,7 @@ console.log(__dirname);
 const app = express()
 const port = 8000
 //here is a change
+//app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../build')));
 
@@ -61,6 +85,34 @@ app.post('/api/removeMovie', async (req, res) => {
   
    res.sendStatus(200);
 })
+
+
+//name
+//fav sport
+//fav movie
+//email
+
+
+app.post('/api/addInfo', jsonParser, async (req, res) => {
+  //const client = new MongoClient(process.env.MONGO_CONNECT);
+  //await client.connect();
+
+  //const db = client.db('customers');
+
+
+  const custInfo = new Info( req.body );
+
+  try {
+    await custInfo.save();
+  }
+  catch (err){
+    res.sendStatus(206);
+    return;
+  }
+
+  res.sendStatus(200);
+})
+
 
 app.post('/api/review', upload.single('movie_poster'),  async (req,res) => {
   const client = new MongoClient(process.env.MONGO_CONNECT);
